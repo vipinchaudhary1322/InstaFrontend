066446e7b0ce72c4ca1889ca36a8f411c4433766
Vipin Chaudhary <108751242+vipinchaudhary1322@users.noreply.github.com>
2023-01-25 10:07:55 +0530

    first commi

---
+++ src/postview/postview.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Header from "../header/header";
+import "./postview.css"
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const PostView = () => {
+
+    const [posts, setpost] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/user")
+            .then((res) => {
+                setpost(res.data.profile)
+            })
+    }, [])
+console.log(posts)
+    return (
+        <div id="insta-container">
+        <Header />
+        {posts.map((post) => {
+            return (<>
+                <div id="post-card">
+                    <h1>{post.name}</h1>
+                    <a href="#"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>
+                    <h2>{post.location}</h2>
+                    <img src={post.image} alt="img"/>
+                    <div id="like-box">
+                        <i class="fa fa-heart" aria-hidden="true"></i>
+                        <i class="fa fa-comment-o" aria-hidden="true"></i>
+                        <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+                        <i class="fa fa-ellipsis-h fa-3x" aria-hidden="true"></i>
+                        <i className="fa fa-bookmark-o" aria-hidden="true"></i>
+                    </div>
+                    <div id="desciption">{post.description}</div>
+                </div>
+
+            </>)
+        })}
+    </div>
+    )
+
+}
+export default PostView;
